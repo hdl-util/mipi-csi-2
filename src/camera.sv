6a24d250471d11e3ac19bed1803853adca3d0c37
Sameer Puri <sameer@users.noreply.github.com>
2020-04-08 11:51:54 -0400

    Fix camera counter logic

--- src/camera.sv
+++ src/camera.sv
@@ -5,14 +5,15 @@ module camera #(
     input logic clock_n,
     input logic [NUM_LANES-1:0] data_p,
     input logic [NUM_LANES-1:0] data_n,
-    // See Section 12 for how this should be parsed
-    output logic [31:0] raw_data = 32'd0,
-    // Corresponding virtual channel for the raw data
-    output logic [1:0] virtual_channel = 2'd0,
+    // Corresponding virtual channel for the image data
+    output logic [1:0] virtual_channel,
     // Total number of words in the current packet
-    output logic [15:0] word_count = 16'd0,
+    output logic [15:0] word_count,
+    // See Section 12 for how this should be parsed
+    output logic [31:0] image_data = 32'd0,
+    output logic [7:0] image_data_type,
     // Whether there is output data ready
-    output logic raw_data_enable = 1'd0
+    output logic image_data_enable
 );
 
 logic [NUM_LANES-1:0] reset = NUM_LANES'(0);
@@ -37,8 +38,9 @@ endgenerate
 
 logic [7:0] packet_header [3:0] = 32'd0;
 assign virtual_channel = packet_header[0][7:6];
-logic [5:0] data_type;
+logic [7:0] data_type;
 assign data_type = packet_header[0][5:0];
+assign image_data_type = data_type;
 assign word_count = {packet_header[2], packet_header[1]}; // Recall: LSB first
 logic [7:0] header_ecc;
 assign header_ecc = packet_header[3];
@@ -48,7 +50,8 @@ logic [16:0] word_counter = 17'd0;
 logic [2:0] data_index = 3'd0;
 
 // Count off multiples of four
-assign raw_data_enable == word_counter > 17'd2 && (word_counter - 17'd2) % 4 == 16'd0;
+// Shouldn't be the first byte
+assign image_data_enable = data_type >= 8'h18 && data_type <= 8'h2F && word_counter != 17'd0 && data_index == 3'd0;
 
 integer j;
 always @(posedge clock_p or posedge clock_n)
@@ -63,26 +66,19 @@ begin
                 packet_header[header_index] = data[j];
                 header_index = header_index + 1'd1;
             end
-            else if (data_type > 8'h0F) // Long packet receive
+            else // Long packet receive
             begin
-                if (header_index == 3'd4)
+                // Image data (YUV, RGB, RAW)
+                if (data_type >= 8'h18 && data_type <= 8'h2F && word_counter < word_count)
                 begin
-                    word_counter = word_count + 17'd1; // Accounts for 2 additional bytes at the end from packet footer
-                    header_index = 3'd5;
-                end
-                else
-                    word_counter = word_counter - 17'd1;
-
-                // Raw data
-                if (word_counter >= 17'd2)
-                begin
-                    raw_data[data_index] = data[j];
-                    data_index = data_index + 2'd1; // Wrap-around 4 byte
+                    image_data[data_index] = data[j];
+                    data_index = data_index + 3'd1; // Wrap-around 4 byte counter
                 end
                 // Footer
                 else
                 begin
                 end
+                word_counter = word_counter + 17'd1;
             end
         end
     end
@@ -91,22 +87,20 @@ begin
     for (j = 0; j < NUM_LANES; j++)
     begin
         if (data_type <= 8'h0F && header_index + 3'(j) >= 3'd4 && !reset[j]) // Reset on short packet end
-        begin
             reset[j] <= 1'b1;
-        end
-        // else if (header_index >= 3'd3 && word_counter)
-        else if (header_index == 3'd5 && word_counter <= 17'(j) && !reset[j]) // Reset on long packet end
+        else if (header_index + word_counter + 17'(j) >= 17'(word_count) + 17'd2 + 3'd4 && !reset[j]) // Reset on long packet end
             reset[j] <= 1'b1;
         else // No reset otherwise
             reset[j] <= 1'b0;
     end
-    // State reset (next clock)
+    // Synchronous state reset (next clock)
     if (reset[0]) // Know the entire state is gone for sure if the first lane resets
     begin
-        packet_header <= 32'd0;
-        header_index <= 4'd0;
-        word_counter <= 17'd0;
-        data_index <= 4'd0;
+        packet_header = 32'd0;
+        image_data = 32'd0;
+        header_index = 3'd0;
+        word_counter = 17'd0;
+        data_index = 4'd0;
     end
 end
 
